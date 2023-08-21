4faeb0e1911749e689005f95df4313346e6e9e3e
Renwar-P <>
2023-08-21 12:02:18 +0000

    add inital functions to js

---
+++ js/script.js
@@ -0,0 +1,7 @@
+// defining the functions
+
+const options=["rock",
+"paper",
+"scissors"];
+const buttons=document.querySelectorAll('.options button');
+const resultElement=document.querySelector('.result');
