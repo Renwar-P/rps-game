abd883e42270113a946b1bca6d0194f0344ccc62
Renwar-P <>
2023-08-21 12:05:27 +0000

    add more functions

--- js/script.js
+++ js/script.js
@@ -5,3 +5,6 @@ const options=["rock",
 "scissors"];
 const buttons=document.querySelectorAll('.options button');
 const resultElement=document.querySelector('.result');
+const playerScoreElement = document.getElementById('player-score');
+const computerScoreElement = document.getElementById('computer-score');
+const reloadButton = document.getElementById('reload');
